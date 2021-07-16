9157007a224fa9c00da291da0a22be65999674d6
Ifeoma Jonah <ifyajoke@gmail.com>
2021-07-15 20:17:16 -0700

    desktop and perfect mobile

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 
 var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// hamburger toggle
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav_list");
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+hamburger.addEventListener("click", mobileMenu);
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+function mobileMenu() {
+    console.log("v");
+    navMenu.classList.toggle("active");
 }
